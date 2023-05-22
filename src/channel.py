c8c1cd8d44d9713644f8ebdbb67c7a97129114cb
vitali <sh015@mail.ru>
2023-05-22 23:47:07 +0300

    1

--- src/channel.py
+++ src/channel.py
@@ -2,11 +2,36 @@
 
 class Channel:
     """Класс для ютуб-канала"""
-
+    api_key: str = os.getenv('YouTube API')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.__channel_id = channel_id
 
-    def print_info(self) -> None:
+    def __str__(self):
+        return f'{self.title}({self.url})'
+
+    def __add__(self, other):
+        return int(self.subs) + int(other.subs)
+
+    def __sub__(self, other):
+        return int(self.subs) - int(other.subs)
+
+    def __gt__(self, other):
+        return int(self.subs) > int(other.subs)
+
+    def __ge__(self, other):
+        return int(self.subs) >= int(other.subs)
+
+    def __lt__(self, other):
+        return int(self.subs) < int(other.subs)
+
+    def __le__(self, other):
+        return int(self.subs) <= int(other.subs)
+
+ def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        # print(json.dumps(channel, indent=2, ensure_ascii=False))
+        return channel
