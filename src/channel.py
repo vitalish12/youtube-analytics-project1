c20a86fbec976f25ab339815d32dedad3adb1f04
vitali <sh015@mail.ru>
2023-05-25 03:33:00 +0300

    3

--- src/channel.py
+++ src/channel.py
@@ -6,110 +6,76 @@ class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YouTube API')
     youtube = build('youtube', 'v3', developerKey=api_key)
-    
+
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.__channel_id = channel_id
-
-    def __str__(self):
-        return f'{self.title}({self.url})'
-
-    def __add__(self, other):
-        return int(self.subs) + int(other.subs)
-
-    def __sub__(self, other):
-        return int(self.subs) - int(other.subs)
-
-    def __gt__(self, other):
-        return int(self.subs) > int(other.subs)
-
-    def __ge__(self, other):
-        return int(self.subs) >= int(other.subs)
-
-    def __lt__(self, other):
-        return int(self.subs) < int(other.subs)
-
-    def __le__(self, other):
-        return int(self.subs) <= int(other.subs)
+        self._channel_id = channel_id  # id канала
+        self.channel = self.youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
+        self.title: str = self.channel['items'][0]['snippet']['title']  # название канала
+        self.description = self.channel['items'][0]['snippet']['description']  # описание канала
+        self.url = f"https://www.youtube.com/channel/{self._channel_id}"  # ссылка на канал
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']  # количество подписчиков
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']  # количество видео
+        self.viewCount = self.channel['items'][0]['statistics']['viewCount']  # общее количество просмотров
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         # print(json.dumps(channel, indent=2, ensure_ascii=False))
         return channel
+
     @classmethod
     def get_service(cls):
-        return build('youtube', 'v3', developerKey=cls.api_key)
+        """Класс-метод `get_service()`, возвращающий объект для работы с YouTube API"""
+        return cls.youtube
 
-    @property
-    def title(self):
-        return self.print_info()["items"][0]["snippet"]["title"]
 
-    @property
-    def description(self):
-        return self.print_info()["items"][0]["snippet"]["description"]
-
-    @property
-    def url(self):
-        return f'https://www.youtube.com/channel/{self.__channel_id}'
-
-    @property
-    def subs(self):
-        return self.print_info()["items"][0]["statistics"]["subscriberCount"]
-
-    @property
-    def video_count(self):
-        return self.print_info()["items"][0]["statistics"]["videoCount"]
-
-    @property
-    def viewers(self):
-        return self.print_info()["items"][0]["statistics"]["viewCount"]
-
-    @property
-    def channel_id(self):
-        return self.__channel_id
-        
-        
-    @classmethod
-    def get_service(cls):
-        return build('youtube', 'v3', developerKey=cls.api_key)
-
-    @property
-    def title(self):
-        return self.print_info()["items"][0]["snippet"]["title"]
-
-    @property
-    def description(self):
-        return self.print_info()["items"][0]["snippet"]["description"]
-
-    @property
-    def url(self):
-        return f'https://www.youtube.com/channel/{self.__channel_id}'
-
-    @property
-    def subs(self):
-        return self.print_info()["items"][0]["statistics"]["subscriberCount"]
-
-    @property
-    def video_count(self):
-        return self.print_info()["items"][0]["statistics"]["videoCount"]
-
-    @property
-    def viewers(self):
-        return self.print_info()["items"][0]["statistics"]["viewCount"]
-
-    @property
-    def channel_id(self):
-        return self.__channel_id      
-        
     def to_json(self, filename):
-        channel_dict = {"id": self.__channel_id,
-                        "title": self.title,
-                        "video_count": self.video_count,
-                        "url": self.url,
-                        "description": self.description
-                        }
-        with open(filename, 'w', encoding='utf-8') as f:
-            json.dump(channel_dict, f, indent=2, ensure_ascii=False)          
-        
+        """Метод `to_json()`, сохраняющий в файл значения атрибутов экземпляра Channel"""
+
+        data = {'channel_id': self._channel_id,
+                'channel': self.channel,
+                'title': self.title,
+                'description': self.description,
+                'url': self.url,
+                'subscriberCount': self.subscriber_count,
+                'video_count': self.video_count,
+                'viewCount': self.viewCount}
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+def __str__(self):
+    return f"<{self.title}> (<{self.url}>)"
+
+
+def __add__(self, other):
+    """Метод для операции сложения"""
+    return int(self.subscriber_count) + int(other.subscriber_count)
+
+
+def __sub__(self, other):
+    """Метод для операции вычитания"""
+    return int(self.subscriber_count) - int(other.subscriber_count)
+
+
+def __lt__(self, other):
+    """Метод для операции сравнения «меньше»"""
+    return int(self.subscriber_count) < int(other.subscriber_count)
+
+
+def __le__(self, other):
+    """Метод для операции сравнения «меньше или равно»"""
+    return int(self.subscriber_count) <= int(other.subscriber_count)
+
+
+def __gt__(self, other):
+    """Метод для операции сравнения «больше»"""
+    return int(self.subscriber_count) > int(other.subscriber_count)
+
+
+def __ge__(self, other):
+    """Метод для операции сравнения «больше или равно»"""
+    return int(self.subscriber_count) >= int(other.subscriber_count)
 
